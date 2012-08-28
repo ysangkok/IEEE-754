9e6ca40b8a6e1094db387c480bc7cc81c002cf59
Janus <ysangkok@gmail.com>
2012-08-29 01:57:29 +0200

    broke 1/3

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -ex
-EMCC_DEBUG=1 ~/emscripten/emcc -O2 -s EXPORTED_FUNCTIONS="['_btd2','_dtb2']" -s PRECISE_I32_MUL=1 -I ~/gmp.js src/concat.links.c libgmp.a -o concat.links.js
+EMCC_DEBUG=1 ~/emscripten/emcc -O0 -g -s EXPORTED_FUNCTIONS="['_btd2','_dtb2']" -s PRECISE_I32_MUL=1 -I ~/gmp.js src/concat.links.c libgmp.a -o concat.links.js
