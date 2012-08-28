1b1b44dbf1ff52a36224d5c4ea3cad0cdbe98f7a
Janus <ysangkok@gmail.com>
2012-08-29 01:22:17 +0200

    build in one js file, compile with -O2, simplify building (no ant), add casperJS tests (not running for emscripten version yet)

---
+++ casperjs_tests/decimaltest.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var utils = require("utils");
+
+var casper = require('casper').create();
+
+
+function testNumber(input, output) {
+	this.input = input;
+	this.output = output;
+	if (input.click === undefined) input.click=["#force-auto"];
+	this.results = [];
+}
+
+var arr = [
+  new testNumber({entered:'1.0625'}, [{key:"#binary_value", value: '1.0001'}]),
+  new testNumber({entered:'1.25'}, [{key:"#binary_value", value: '1.01'}])
+];
+
+var percasper = function(pagenr) {
+arr.map(function(i) {
+casper.then(function() {
+console.log("pagenr: " + pagenr);
+});
+var input = i.input;
+var output = i.output;
+var oldvalue;
+
+//utils.dump(input);
+//utils.dump(output);
+
+casper.then(function() { console.log(input.entered); });
+
+casper.thenEvaluate(function(term) {
+        document.getElementById('value-entered').setAttribute("value",term);
+},{term: input.entered});
+
+casper.then(function(term) {
+        oldvalue = this.fetchText(output[0].key);
+	console.log("oldvalue: " + oldvalue);
+	var outer = this;
+        input.click.map(function(j) { outer.click(j); });
+});
+
+casper.then(function(){console.log("wait for idle");});
+casper.waitWhileVisible(".idle");
+casper.then(function(){console.log("waiting for new result");});
+casper.waitFor(function check() {
+    var value = this.fetchText(output[0].key);
+    console.log(value);
+    return value !== oldvalue;
+}, function then() {}, function timeout(){this.test.assertTrue(false,"timeout");});
+
+casper.then(function() {
+    var outer = this;
+    output.map( function (v) {
+      outer.test.assertEquals(outer.fetchText(v.key), v.value);
+    });
+});
+
+/*
+casper.then(function() {
+    this.captureSelector(i + '.ref.png', 'html');
+});
+*/
+casper.then(function() {
+	i.results.push(this.getPageContent());
+});
+});
+};
+
+
+casper.start('http://localhost:8800/IEEE-754/');
+percasper(0);
+casper.thenOpen('http://localhost:8800/I2/');
+percasper(1);
+
+casper.run(function() {
+	this.test.renderResults(true);
+});
+
